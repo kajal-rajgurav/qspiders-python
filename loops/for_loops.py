634cd6b44f2a6f5dbd609906c50245bfc62dad12
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-03 19:45:43 +0530

    python practice

---
+++ loops/for_loops.py
@@ -0,0 +1,103 @@
+# wapt access the values from the multiple datatypes.
+
+# st={10,20,30,40,80}
+# for i in st:
+#     print(i)
+
+# lst=[10,30,'apple',40]
+# for value in lst:
+#     print(value)
+
+# tp=(1,2,3,4,5)
+# for item in tp:
+#     print(item)
+
+# s={10,20,30,40,80}
+# for element in s:
+#     print(element)
+
+# d={'a':1,'b':2,'c':3}
+# for k in d:
+#     print(k)
+#     print(d[k])
+
+
+
+
+# wapt extract to all the even from list.
+
+
+# lst=[1,2,3,4,5,8,6,10,20]
+# lst_evens=[]
+# for i in lst:
+#     if(i%2==0):
+#         lst_evens.append(i)
+# print(lst_evens)
+        
+# wapt to exract the vowels from the given string .
+# st='happy new year'
+# new_st=''
+# for i in st:
+#     if i in 'AEIOUaeiou':
+#         new_st+=i
+# print(new_st)
+
+# wapt extract upper case ,lowercase,digit,speacial separetely
+
+
+
+# greet='Happy New Year!!@2026'
+# upper=''
+# lower=''
+# digit=''
+# special=''
+# for char in greet:
+#     if char.isupper():
+#         upper+=char
+#     elif char.islower():
+#         lower+=char
+#     elif char.isdigit():
+#         digit+=char
+#     else:
+#         special+=char
+# print(upper,lower,digit,special)
+     
+        
+# wapt to extract only string data from list of data.
+
+# data=[2,2.4,True,'apple',3+4j,'google',60,'microsoft']
+# st=[]
+# for i in data:
+#     if type(i)==str:
+#         st.append(i)
+# print(st)
+
+
+# wapt get to the only sum integer prensent in the tuple.
+
+
+# tp=(2,'yash',3,True,5.6,'Tarak',5,6+4j)
+# Sum=0
+# for t in tp:
+#     if type(t)==int:
+#         Sum+=t
+# print(Sum)
+
+
+# wapt reverse the string 
+ 
+
+s='HAPPY'
+rev=''
+for i in s:
+    rev=i+rev
+print(rev)
+
+
+var=121
+rev=var
+pall=0
+for i in var:
+    
+
+
