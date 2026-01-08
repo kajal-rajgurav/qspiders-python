56cd6e8a671d3bea8f6cd594405374f9c7ec35b3
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-08 21:19:45 +0530

    practice

--- loops/intermidiate_loops/pass_state.py
+++ loops/intermidiate_loops/pass_state.py
@@ -1,4 +1,24 @@
 # wapt to run the loop contineusouly untill user enters proper password.
-my_pass=eval(input("enter your password"))
-saved_password='py@123'
-while 
+
+# saved_password='Py@123'
+# while True:
+#     my_pass=input("enter your password:")
+#     if my_pass==saved_password:
+#         print("login succesfull...")
+#         break
+#     else:
+#         print("wrong password")
+
+# wapt to guess the number.
+
+saved_number=19
+while True:
+    num=int(input("enter your guessing number:-"))
+    if num==saved_number:
+        print("wow guess corrected")
+        break
+    elif num>saved_number:
+        print("enter lesser than this number ")
+    else:
+        print("enter the greater than this number")
+
