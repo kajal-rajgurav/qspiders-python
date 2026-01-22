dc90f6dadf7d367f93990d4cd2d375ff337d2ea6
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-22 10:34:23 +0530

    python test pract

---
+++ functions/unpacked/demo.py
@@ -0,0 +1,10 @@
+# normal way
+# once return is executed inside function it will terminate further execution or it not allow the any other statement after  execution  ######
+#  wapt to check given no is even or not if it even retrun true if is not retrun false .
+def iseven(number):
+    return number%2==0
+print(iseven(5))
+print(iseven(4))
+
+
+
