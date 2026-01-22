0bc7419ce02cf13ba950a83d52c403b64f4efccc
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-22 23:38:04 +0530

    python test pract

--- functions/unpacked/demo.py
+++ functions/unpacked/demo.py
@@ -1,14 +1,25 @@
 # normal way
 # once return is executed inside function it will terminate further execution or it not allow the any other statement after  execution  ######
 #  wapt to check given no is even or not if it even retrun true if is not retrun false .
-def iseven(number):
-    return number%2==0
-print(iseven(5))
-print(iseven(4))
+# def iseven(number):
+#     return number%2==0
+# print(iseven(5))
+# print(iseven(4))
 
-# wapt to print list of even numbers between the range based on user enter limit as an arguments
+# # wapt to print list of even numbers between the range based on user enter limit as an arguments
 
-def lst_even_numbers(start,end):
-    return [numbers for numbers in range(start,end) if numbers%2==0]
-print(lst_even_numbers(1,50))
-print(lst_even_numbers(100,200))
+# def lst_even_numbers(start,end):
+#     return [numbers for numbers in range(start,end) if numbers%2==0]
+# print(lst_even_numbers(1,50))
+# print(lst_even_numbers(100,200))
+
+######### factorial#######
+# 
+def factorial(numbers):
+    fact=1
+    for num in range(1,numbers):
+        fact*=num
+    return fact
+print(factorial(4))
+print(factorial(5))
+print(factorial(6))
