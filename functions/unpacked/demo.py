ee1b999d1247cc31db9ad25a44330fb303abe179
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-22 10:44:33 +0530

    python test pract

--- functions/unpacked/demo.py
+++ functions/unpacked/demo.py
@@ -6,5 +6,9 @@ def iseven(number):
 print(iseven(5))
 print(iseven(4))
 
+# wapt to print list of even numbers between the range based on user enter limit as an arguments
 
-
+def lst_even_numbers(start,end):
+    return [numbers for numbers in range(start,end) if numbers%2==0]
+print(lst_even_numbers(1,50))
+print(lst_even_numbers(100,200))
