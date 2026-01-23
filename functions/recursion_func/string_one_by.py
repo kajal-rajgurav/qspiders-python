0fc15c71d1772d02eadad5b5843fb79cb37ee56f
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-23 10:35:28 +0530

    python test pract

--- functions/recursion_func/string_one_by.py
+++ functions/recursion_func/string_one_by.py
@@ -1,2 +1,11 @@
 # wapt print one by one char from given string.
 st='happy'
+def chr_str(st,i=0):
+    if i<len(st):
+        print(st[i])
+        chr_str(st,i=i+1)
+chr_str("happy")
+chr_str([1,2,34,5])
+
+
+# wapt to extract all the string data from the list using Recursion.
