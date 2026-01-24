07ff71ece245be85d574a1a2b5459b357d445397
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-24 22:21:14 +0530

    daily practice about recursion

--- functions/recursion_func/even_num.py
+++ functions/recursion_func/even_num.py
@@ -1,9 +1,16 @@
 # wapt even   1 to 100 using recursion.
-def numbers(num=1):
-    if num<=100:
-     if num%2==0:
+def numbers(num):
+    if num>100:
+        return
+    if num%2==0:
+        print(num, end=" ")
+    numbers(num+1)
+numbers(1)
+# def print_even(n):
+#     if n > 100:
+#         return
+#     if n % 2 == 0:
+#         print(n, end=" ")
+#     print_even(n + 1)
 
-        print(num)
-        num+=1
-        numbers(num)
-numbers()
+# print_even(1)
