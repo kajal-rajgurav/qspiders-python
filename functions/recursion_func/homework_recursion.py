e6e5b73e46ac82c8a8e72b6bdc7b42603af49dcc
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-24 23:07:45 +0530

    daily practice about recursion

--- functions/recursion_func/homework_recursion.py
+++ functions/recursion_func/homework_recursion.py
@@ -1,13 +1,40 @@
 data=[87,"hai","python",8.3,"congratulations"]
 new_lst=[]
+vowel = 'aeiou'
+resultDict = {}
+
 for i in data:
     if type(i)==str:
         new_lst.append(i)
-        print("yes it string")
-    else:
-        print("no a string")
+
+print(new_lst)
+
+for word in new_lst:
+    count=0
+    for ch in word.lower():
+        if ch in vowel:
+            count +=1
+    
+    resultDict[word.lower()] = count
+
+print(resultDict)
+
+
+
+
+
+
+
+
+
+
+
+
 # wapt longest word in the string
-greet='happy makar sankranti whishing you joy prosperity health and success'
-words=greet.split()
-words.sort(key=len)
-print(words[-1])
+# greet='happy makar sankranti whishing you joy prosperity health and success'
+# words=greet.split()
+# words.sort(key=len)
+# print(words[-1])
+
+# st="my name is kajal"
+# print({chr:st.count(chr) for chr in st})
