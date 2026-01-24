fd49472765af1e909cdf2d822bbed8a254cf512a
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-24 22:05:56 +0530

    daily practice about recursion

--- functions/recursion_func/string_one_by.py
+++ functions/recursion_func/string_one_by.py
@@ -9,3 +9,18 @@ chr_str([1,2,34,5])
 
 
 # wapt to extract all the string data from the list using Recursion.
+lst=[10,'apple',30,'google',45,'tcs','ibm',5.6,True]
+out=[]
+i=0
+while i<len(lst):
+    if type(lst[i])==str:
+        out.append(lst[i])
+    i+=1
+print(out)
+def str_elements(lst,out=[],i=i+1):
+    if i<len(lst):
+        if type(lst[i])==str:
+          out.append(lst[i])
+        str_elements(lst,out,i=i+1)
+        return out
+print(str_elements([10,'apple',30,'google',45,'tcs','ibm',5.6,True]))
