a6f1943b867db007cc7709194cfd5469186a82ec
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-25 23:43:13 +0530

    daily practice about recursion

--- functions/recursion_func/homework_recursion/sumof_firstn_num.py
+++ functions/recursion_func/homework_recursion/sumof_firstn_num.py
@@ -5,3 +5,10 @@ def sum_n(n):
     return n + sum_n(n-1)
 
 print(sum_n(5))
+# how its working 
+# sum_digits(123)
+# = 3 + sum_digits(12)
+# = 3 + 2 + sum_digits(1)
+# = 3 + 2 + 1 + sum_digits(0)
+# = 6
+# 👉 Example: 123 → 1 + 2 + 3 = 6
