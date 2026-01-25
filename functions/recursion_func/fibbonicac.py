d1fc1437e2dcb7a551fa22a0568535dcad11e522
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-25 23:36:28 +0530

    recursion homewrok practice about fibbonicac

--- functions/recursion_func/fibbonicac.py
+++ functions/recursion_func/fibbonicac.py
@@ -8,4 +8,21 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
 
 print(fibonacci(6))
+# how its works
+# fibonacci(5)
+# = fibonacci(4) + fibonacci(3)
+
+# fibonacci(4)
+# = fibonacci(3) + fibonacci(2)
+# = 2 + 1
+# = 3
+
+# fibonacci(3)
+# = fibonacci(2) + fibonacci(1)
+# = 1 + 1
+# = 2
+
+# So,
+# fibonacci(5) = 3 + 2 = 5
+
 
