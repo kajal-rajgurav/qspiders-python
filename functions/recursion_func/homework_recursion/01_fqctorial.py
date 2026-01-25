95505a613da7de4036ff7e72953c56095773c5dd
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-25 23:30:41 +0530

    recursion homewrok practice about factorial

---
+++ functions/recursion_func/homework_recursion/01_fqctorial.py
@@ -0,0 +1,8 @@
+# Example 1: Factorial using Recursion
+def factorial(n):
+    if n == 0 or n == 1:   # Base case
+        return 1
+    else:
+        return n * factorial(n - 1)  # Recursive call
+
+print(factorial(5))
