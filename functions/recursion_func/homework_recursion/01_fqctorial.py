58476fe4930445d6415e5308ecf4414809288a28
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-25 23:32:04 +0530

    recursion homewrok practice about factorial

--- functions/recursion_func/homework_recursion/01_fqctorial.py
+++ functions/recursion_func/homework_recursion/01_fqctorial.py
@@ -6,3 +6,11 @@ def factorial(n):
         return n * factorial(n - 1)  # Recursive call
 
 print(factorial(5))
+# how itas  works
+# factorial(5)
+# = 5 * factorial(4)
+# = 5 * 4 * factorial(3)
+# = 5 * 4 * 3 * factorial(2)
+# = 5 * 4 * 3 * 2 * factorial(1)
+# = 120
+
