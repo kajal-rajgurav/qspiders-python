088e450baaf6e7bce32a077d46cf4ae934952078
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-26 23:21:56 +0530

    python test pract

--- functions/user_defiendfunctions/decorators/02_add_func.py
+++ functions/user_defiendfunctions/decorators/02_add_func.py
@@ -3,3 +3,17 @@ def addition(a, b):
 
 getAddition = addition(23, 11)
 print(getAddition)
+
+
+# decorators with arguments
+def smart_decorator(func):
+    def wrapper(name):
+        print("Good Morning")
+        func(name)
+    return wrapper
+
+@smart_decorator
+def greet(name):
+    print("Hello", name)
+
+greet("Kajal")
