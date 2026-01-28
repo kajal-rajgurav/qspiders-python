99b8a9d7168dd51422757b688319b634a5ff6d36
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-28 23:33:23 +0530

    python test pract

--- functions/user_defiendfunctions/decorators/exeption handling/multiple_eg.py
+++ functions/user_defiendfunctions/decorators/exeption handling/multiple_eg.py
@@ -6,3 +6,12 @@ except ValueError:
     print("Value error occurred")
 except ZeroDivisionError:
     print("Division by zero error")
+
+# using else.
+try:
+    x = int(input("Enter a number: "))
+    y = 10 / x
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+else:
+    print("Result:", y)
