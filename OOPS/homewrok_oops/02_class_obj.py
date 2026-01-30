c1f2fbfa5de5deacfdda60bb913405ed3307a802
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-30 23:23:47 +0530

    ooops concept learning about creating the class and objects

---
+++ OOPS/homewrok_oops/02_class_obj.py
@@ -0,0 +1,18 @@
+class car:
+    c_brand='tata'
+    c_model='toyota'
+    year=2011
+    ceo='ratan tata'
+
+# perspm
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+
+print(p1.name)
+print(p1.age)
