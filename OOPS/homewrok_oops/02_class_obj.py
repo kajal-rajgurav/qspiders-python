1a7132e6def18cc15e59dd906c25ad0af21ba436
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-30 23:25:01 +0530

    ooops concept learning about creating the class and objects using init keyword

--- OOPS/homewrok_oops/02_class_obj.py
+++ OOPS/homewrok_oops/02_class_obj.py
@@ -16,3 +16,14 @@ p1 = Person("Emil", 36)
 
 print(p1.name)
 print(p1.age)
+
+# created class using __init__ keyword
+class Person:
+  pass
+
+p1 = Person()
+p1.name = "Tobias"
+p1.age = 25
+
+print(p1.name)
+print(p1.age)
