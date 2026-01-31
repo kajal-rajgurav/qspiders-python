28153fd93f434c1b2306c15a81f35f3b76f3dec3
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-31 23:36:59 +0530

    doing own practice about class and object

--- OOPS/homewrok_oops/obj_func.py
+++ OOPS/homewrok_oops/obj_func.py
@@ -1,3 +1,10 @@
+#Explanation
+
+# Student → Class (blueprint)
+
+# s1, s2 → Objects
+
+# self → Current object ko refer karta hai
 class student:
     def __init__(self,name,roll_no,address):
         self.name=name
@@ -12,5 +19,21 @@ stud1=student("kajal",23,"andheri")
 stud2=student("vidya",24,"malad")
 stud1.display()
 stud2.display()
+# Example 2: Bank Account Class
+class BankAccount:
+    def __init__(self, account_holder, balance):
+        self.account_holder = account_holder
+        self.balance = balance
 
+    def deposit(self, amount):
+        self.balance += amount
+        print("Deposited:", amount)
+
+    def show_balance(self):
+        print("Balance:", self.balance)
+
+# Object
+acc1 = BankAccount("Kajal", 5000)
+acc1.deposit(2000)
+acc1.show_balance()
     
