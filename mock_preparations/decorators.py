ce96eaf949766d3bd4926cb99f4f3f555a1f6802
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-02-16 22:57:53 +0530

    im learing decorators

---
+++ mock_preparations/decorators.py
@@ -0,0 +1,17 @@
+# 1. What is a Decorator?
+
+# 👉 A decorator is used to modify a function without changing its code.
+
+# 💡 Real use:
+
+# Login check
+
+# Logging
+
+# Timing execution/
+
+
+
+# 2. How to create a Decorator?
+#   decorators are functions that make use of hgher order functions and closures.
+
