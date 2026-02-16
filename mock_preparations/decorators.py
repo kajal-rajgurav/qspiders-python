316a5f687d5183a3b3c31877b741d877f2306581
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-02-16 23:14:08 +0530

    im learing decorators

--- mock_preparations/decorators.py
+++ mock_preparations/decorators.py
@@ -15,3 +15,15 @@
 # 2. How to create a Decorator?
 #   decorators are functions that make use of hgher order functions and closures.
 
+# REAL USE CASE (Login Check)
+def login_required(func):
+    def wrapper():
+        print("Checking login...")
+        func()
+    return wrapper
+
+@login_required
+def dashboard():
+    print("Dashboard loaded")
+
+dashboard()
