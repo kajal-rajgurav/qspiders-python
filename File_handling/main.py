4a33b36a6d655cb15e3e013d68e3a1b738e8dc6b
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-02-17 10:13:07 +0530

    file handling

--- File_handling/main.py
+++ File_handling/main.py
@@ -1,9 +1,20 @@
-file=open('sample.txt','w')
-file.write('python is esay\n')
-file.writelines(['abc\n','bcd\n','bv'])
-file.close()
+# file=open('sample.txt','w')
+# file.write('python is esay\n')
+# file.writelines(['abc\n','bcd\n','bv'])
+# file.close()
 
-###############################################
-with open('demo.txt','w')as f:
-    f.write('kajal\n')
-    f.writelines(['abc\n','bcd\n','bv'])
+# #####################writting another way to file ##########################
+# with open('demo.txt','w')as f:
+#     f.write('kajal\n')
+#     f.writelines(['abc\n','bcd\n','bv'])
+########################reading to file
+
+# with open(r"C:\Users\rajgu\OneDrive\Desktop\Qspiders_practice\python\demo.txt",'r')as file:
+#     # print(file.read())
+#     # print(file.readline())
+#     print(file.readlines())
+
+##########appeding to a file
+with open('demo.txt','a')as f:
+    f.write('\nkajal\n')
+    f.writelines(['abc\n','adsvd\n','adfs\n'])
