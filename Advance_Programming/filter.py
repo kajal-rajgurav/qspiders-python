5c12ae1bf2830f96f78a4988ee82e11708c51e58
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-02-21 23:03:24 +0530

    learning about filter function

---
+++ Advance_Programming/filter.py
@@ -0,0 +1,18 @@
+# 2️⃣ filter() Function
+# 👉 What is filter()?
+
+# filter() is used to select elements based on condition.
+
+# Simple words:
+
+# “Condition check karo, jo pass ho usko rakho”
+
+# 🔹 Example 1: Get even numbers
+numbers = [1, 2, 3, 4, 5, 6]
+
+def is_even(x):
+    return x % 2 == 0
+
+result = filter(is_even, numbers)
+
+print(list(result))
