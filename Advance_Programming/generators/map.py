c1afa44afa51f35bea870aa9f37e59972969b1bd
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-02-21 23:00:39 +0530

    learning about map function

--- Advance_Programming/generators/map.py
+++ Advance_Programming/generators/map.py
@@ -16,3 +16,10 @@ def square(x):
 result = map(square, numbers)
 
 print(list(result))
+
+# Example 2: Using lambda (short method)
+numbers = [1, 2, 3, 4]
+
+result = map(lambda x: x * 2, numbers)
+
+print(list(result))
