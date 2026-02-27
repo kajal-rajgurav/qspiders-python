8d48a5a55828f2ce57411d695e117d67a6427434
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-02-27 22:25:13 +0530

    arrays

---
+++ Array/01_array.py
@@ -0,0 +1,13 @@
+import numpy  as np
+# 1. create a 1D array of integers from 0 to 9. 
+arr=np.array([0,1,2,3,4,5,6,7,8,9])
+print(arr[0])
+print(arr[1])
+print(arr[2])
+print(arr[3])#index
+print(arr[4])
+print(arr[5])
+print(arr[6])
+print(arr[7])
+print(arr[8])
+print(arr[9])
